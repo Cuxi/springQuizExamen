6f82686938e1c7f747453943846f05fe3a32cae2
Cuxi <marferyan@gmail.com>
2017-03-14 15:41:57 +0100

    Update PreguntaController.java

--- project/src/main/java/controller/PreguntaController.java
+++ project/src/main/java/controller/PreguntaController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.context.annotation.Scope;
 import redis.clients.jedis.Jedis;
 import service.PreguntaServiceImpl;
